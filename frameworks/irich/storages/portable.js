89e0f54fd0b36a61ca3a30fcbe4161937847c232
saki <sakinijino@gmail.com>
2010-12-30 21:33:06 +0800

    remove duplicated buffer in portable storage adapter

--- frameworks/irich/storages/portable.js
+++ frameworks/irich/storages/portable.js
@@ -1,83 +1,28 @@
-iRich.PortableStorageAdapter = SCUDS.LocalStorageAdapter.extend({
- 
+iRich.PortableStorageAdapter = SCUDS.LocalStorageAdapter.extend({ 
   localStorageKey: 'irich.portable',
-  contentItemKey: 'id',
-
-  _deserializeHash: function(key) {
-    var results;
-    if (SC.empty(key)) key = this.localStorageKey;
-    try {
-      var data = window.iRich.portableStorage.getItem(key) || '{}'
-      results = SC.json.decode(data) || {};
-    } catch(e) {
-      SC.Logger.warn('Error during deserialization of records as hash. data: '+data);
-      this.nuke();
-      results = {};
-    }
-    return results;
-  },
-
-  _serializeHash: function(data, key) {
-    if (SC.empty(key)) key = this.localStorageKey;
-    return window.iRich.portableStorage.setItem(key, SC.json.encode(data));
-  },
 
   save: function(obj, key) {
-    if (obj instanceof Array) {
-      return this._saveAll(obj, key);
-    } else if (!obj) {
-      SC.Logger.error('Error writing record to cache: Record is null or undefined.');
-      return NO;
-    }
-
-    if (SC.empty(key)) key = obj[this.contentItemKey];
-    
-    if (key) {
-      var id, data = this._deserializeHash();
-      data[key] = obj
-      this._serializeHash(data);
-      return YES;
-    } else {
-      SC.Logger.error('Error writing record to cache: Invalid key.');
-      return NO;
-    }
+    if (SC.empty(key) || !obj) return NO;
+    window.iRich.portableStorage.setItem(key, obj);
+    return YES
   },
 
-  _saveAll: function(objs, keys) {
-    var i, key, length = objs.length || 0;
-
-    var data = this._deserializeHash() || {};
-
-    for (i = 0; i < length; i++) {
-       key = (keys && keys.length) ? keys[i] : this.contentItemKey;
-       data[objs[i][key]] = objs[i];
-    }
-    this._serializeHash(data);
-  },
-
-  get: function(id) {
-    var data = this._deserializeHash() || {};
-    var rec = data[id];
-    return rec ? rec : null;
+  get: function(key) {
+    return window.iRich.portableStorage.getItem(key)
   },
 
   getAll: function() {
-    var data =  this._deserializeHash() || {};
-    var ret = []
-    for (var n in data) {
-      ret.push(data[n])
-    }
-    return ret
+    return window.iRich.portableStorage.getItems()
   },
 
-  remove: function(id) {
-    var data = this._deserializeHash() || {};
-    delete data[id];
-    this._serializeHash(data);
+  remove: function(key) {
+    if (SC.empty(key)) return NO;
+    window.iRich.portableStorage.removeItem(key)
+    return YES
   },
 
   nuke: function() {
-    window.iRich.portableStorage.removeItem(this.localStorageKey);
-    window.iRich.portableStorage.removeItem(this._bufferKey);
+    window.iRich.portableStorage.removeItems();
+    return YES
   }
 });
